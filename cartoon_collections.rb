d5e4b565ea78e7cc03bf3a86681ce21e956014d8
Luis Zabala <luis.zabala076@gmail.com>
2019-01-09 19:57:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(calls)
 end
 
 
-def find_the_cheese(cheeses)
+def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheeses_types.find do |cheddar|
     cheese_types.include?(cheddar)
