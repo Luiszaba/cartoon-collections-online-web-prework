6fcefdbf77c06ec478654523e895f871e43fedc7
Luis Zabala <luis.zabala076@gmail.com>
2019-01-09 19:47:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(elements)
 
 def long_planeteer_calls(assorted_words)
   assorted_words = ["two", "go", "industrious", "bop"]
-  assorted_words.any? do |calls| assorted_words.length < 4
+  assorted_words.any? do |calls| assorted_words.length <=4
   end
 end
 
